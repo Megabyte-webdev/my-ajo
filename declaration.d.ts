14a12923dc7f27cbab5b02f477af8b7f53bc1ba8
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-05-15 17:50:14 +0100

    Update declaration.d.ts

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,1 +1,10 @@
+// declarations.d.ts
+
+// Allow importing .jsx files without TypeScript complaints
 declare module '*.jsx';
+
+// Suppress missing modules (only if you absolutely must)
+declare module './pages/ComingSoon';
+declare module '../utils/notifications/OnFailure';
+declare module '../utils/notifications/OnSuccess';
+declare module '../utils/formmaters';
