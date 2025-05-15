23661f495996a7d8c10ed587c218b2dd06e0548e
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-05-15 17:52:04 +0100

    Update custom.d.ts

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,1 +1,12 @@
 declare module 'react-helmet'
+
+// declarations.d.ts
+
+// Allow importing .jsx files without TypeScript complaints
+declare module '*.jsx';
+
+// Suppress missing modules (only if you absolutely must)
+declare module './pages/ComingSoon';
+declare module '../utils/notifications/OnFailure';
+declare module '../utils/notifications/OnSuccess';
+declare module '../utils/formmaters';
