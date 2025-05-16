d5a9454d3ae5fe29fcff7710f11134671b896e83
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-05-16 11:02:43 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,8 @@ const Navbar = ({ register = "/registration", login = "/login" }) => {
     <nav
       className="sticky top-0 left-0 right-0 z-20 max-w-peak min-h-14 w-full mx-auto flex p-3 px-sm md:px-md lg:px-lg justify-between items-center flex-wrap md:flex-nowrap transition-all duration-300 bg-white shadow-md">
       <Link to="/" className="flex justify-center items-center mr-auto">
-        <img src={logo} alt="logo" className="w-28 md:w-32" />
+        {/*<img src={logo} alt="logo" className="w-28 md:w-32" />*/}
+        <h1 className="text-2xl text-white font-semibold">MyÀjo</h1>
       </Link>
 
       <div
